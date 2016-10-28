d287b311c8bdd0736def7b8ed0ca82e69866d7e5
mourad <mourad.arifi@armediacg.com>
2016-10-29 00:52:35 +0100

    Initial commit

---
+++ mod_info.lua
@@ -0,0 +1,8 @@
+name = "Training"
+version = 1
+Author = "The Amaziɣ (aka Phelom)"
+copyright = "The Amaziɣ, 2016"
+description = "yeah yeah..."
+uid = "1A07889D-09FF-407F-8542-C9CCBCB4F964"
+exclusive = false
+ui_only = false
